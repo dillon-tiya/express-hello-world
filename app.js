6c0863e5340d130820a2a3baf401a4cb9d67223c
corinne <corinne.choo@tiya.com>
2023-02-03 18:05:19 +0800

    edited to fit funchat

--- app.js
+++ app.js
@@ -40,4 +40,31 @@ app.use('*', (req,res) => {
     .end()
 })
 
+const redirect = () => {
+  const message = document.querySelector('.message');
+  message.innerText = window.location.hash.substring(1,);
+  const jsonString = atob(message.innerText);
+  console.log(jsonString);
+  const jsonDict = JSON.parse(jsonString);
+  const code = jsonDict.code;
+  const org_scoped_id = jsonDict.org_scoped_id;
+  console.log(code);
+  console.log(org_scoped_id);
+  fetch("https://graph.oculus.com/sso_authorize_code", {
+      method: "POST",
+      body: JSON.stringify({
+          code: code,
+          access_token: 'OC|5755038047868820|add12f26fe71bbaf723c1058e110352e',
+          org_scoped_id: org_scoped_id
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+};
+
+redirect();
+
 module.exports = app
